520330103871d0a7cfcbdfff5fce0d321dd7e3a7
Allison Van Pelt <avantpelt@gmail.com>
2022-12-05 21:20:06 -0500

    Day 3 Puzzles

---
+++ code/day3-part1.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+// Load data from text file
+fs.readFile("../input/day3.txt", (err, data) => {
+    if (err) throw err;
+
+    let rucksacks = data.toString().split(/\n/);
+    let priorities = 0;
+    for (const rucksack of rucksacks) {
+        let compartment1 = rucksack.slice(0, rucksack.length / 2);
+        let compartment2 = rucksack.slice(rucksack.length / 2);
+
+        for (const item of compartment1) {
+            if (compartment2.includes(item)) {
+                priorities += characters.indexOf(item) + 1;
+                break;
+            }
+        }
+    }
+
+    console.log("Priorities total:", priorities);
+});
---
+++ code/day3-part2.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+// Load data from text file
+fs.readFile("../input/day3.txt", (err, data) => {
+    if (err) throw err;
+
+    let rucksacks = data.toString().split(/\n/);
+
+    let priorities = 0;
+    for (let i = 0; i < rucksacks.length; i += 3) {
+        let rucksack1 = rucksacks[i];
+        let rucksack2 = rucksacks[i + 1];
+        let rucksack3 = rucksacks[i + 2];
+
+        for (const item of rucksack1) {
+            if (rucksack2.includes(item) && rucksack3.includes(item)) {
+                priorities += characters.indexOf(item) + 1;
+                break;
+            }
+        }
+    }
+
+    console.log("Priorities total:", priorities);
+});
